c7f5893f00ccc9f0bad720a97f62a40b155dc873
ashen-1-dev <alen2014@gmail.com>
2020-11-19 19:48:23 +0700

    Банк заданий

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/tasks', 'App\Http\Controllers\TaskController@createTask')->name('create_task_route'); // Добавить новое задание
+Route::get('/tasks', 'App\Http\Controllers\TaskController@allTasks')->name('show_tasks_route');    // Просмотреть все задания(неоптимизированно)
+Route::get('/tasks/{taskId}', 'App\Http\Controllers\TaskController@showTask')->name('one_task_route'); // Посмотреть задание {id}
+Route::put('/tasks/{task}', 'App\Http\Controllers\TaskController@editTask')->name('edit_task_route'); // Изменить задание {id}
+Route::delete('/tasks/{task}', 'App\Http\Controllers\TaskController@deleteTask')->name('delete_task_route'); // Удалить задание {id}
+
